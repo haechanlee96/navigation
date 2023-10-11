e7e0461013ae209b94b2af122f1e689e6a0510af
haechanlee96 <131975479+haechanlee96@users.noreply.github.com>
2023-10-11 17:42:29 +0900

    feat : api 호출 기능 구현

---
+++ src/main/java/com/example/navigation/MainService.java
@@ -0,0 +1,10 @@
+package com.example.navigation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+// 네비 경로 찾기
+public class MainService {
+}
